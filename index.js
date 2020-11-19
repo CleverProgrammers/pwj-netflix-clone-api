b40d8ee26fd505a436f94f4e7b05079646e4006b
Nazariy995 <nazariy1995@gmail.com>
2020-11-19 14:27:39 -0800

    ADD: NOV 19 , login and register

--- index.js
+++ index.js
@@ -1,12 +1,76 @@
 const express = require('express') // importing express
 const app = express() // initialize express
 const port = 3000 //setting the port 
+const mongoose = require('mongoose');
+const { Schema } = mongoose; // Grab the schema object from mongoose
+require('dotenv').config()
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nonsu.mongodb.net/netflix-api-db-dev?retryWrites=true&w=majority`, {
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const User = mongoose.model('Users', new Schema(
+  { 
+    name: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  }
+));
+
+app.use(express.json());
 
 // using the get method
 // LOGIC for the Get Request
 // I'm trying to get data
 app.get('/', (req, res) => {
   res.send("Hellow World")
+})
+
+app.post('/register', (req, res)=>{
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  newUser.save((err, user)=>{
+    if(err){
+      console.log(err);
+      res.send(400, {
+        status: err
+      })
+    } else {
+      console.log("all is good");
+      console.log(user);
+      res.send("registered")
+    }
+  })
+})
+
+app.post('/login', (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+  User.findOne({ email: email, password: password }, (err, user)=>{
+    console.log(user);
+    if(user){
+      res.send({
+        status: "valid"
+      });
+    } else {
+      res.send(404, {
+        status: "Not Found"
+      })
+    }
+  })
 })
 
 // start our app
