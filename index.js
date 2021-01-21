77add75c5581b58e9394e681e2ff19302ff69e80
Nazariy995 <nazariy1995@gmail.com>
2021-01-21 12:42:33 -0800

    ADD: mongo env variable

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { Schema } = mongoose; // Grab the schema object from mongoose
 var cors = require('cors');
 require('dotenv').config()
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nonsu.mongodb.net/netflix-api-db-dev?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nonsu.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`, {
   useCreateIndex: true,
   useUnifiedTopology: true,
   useNewUrlParser: true
